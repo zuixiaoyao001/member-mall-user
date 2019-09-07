8a30a47ede0772a86c2388bf1483fc33660dcc03
zuixiaoyao <1248375279@qq.com>
2019-09-07 13:11:52 +0800

    會員商城user

---
+++ src/main/java/com/drawnblue/usercontroller/UserController.java
@@ -0,0 +1,44 @@
+package com.drawnblue.usercontroller;
+
+import com.drawnblue.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+    @Autowired
+    private RestTemplate restTemplate;
+    @Autowired
+    private DiscoveryClient discoveryClient;
+    @GetMapping("/test/{id}")
+    public User findById(@PathVariable Long id) {
+        System.out.println("調用kaishi");
+        ResponseEntity<User> body = this.restTemplate.getForEntity("http://user-service/userService/findById/"+id, User.class);
+        System.out.println("調用完成");
+        return body.getBody();
+    }
+    @GetMapping("/getInstances")
+    public List<ServiceInstance> getInstances(){
+        return this.discoveryClient.getInstances("user-service");
+    }
+    @GetMapping("/test1/{id}")
+    public User findById2(@PathVariable Long id){
+        List<ServiceInstance> instances = discoveryClient.getInstances("user-service");
+        System.out.println("======================"+instances);
+        String  targetURL = instances.stream()
+                .map(instance -> instance.getUri().toString()+"/userService/findById/{id}")
+                .findFirst().orElseThrow(()-> new IllegalArgumentException("当前没有实例"));
+
+        System.out.println(targetURL+"------targetUrl"+"    "+this.restTemplate);
+        ResponseEntity<User> body =  this.restTemplate.getForEntity(targetURL,User.class,1);
+        return body.getBody();
+    }
+}
